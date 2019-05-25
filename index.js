25662b8325ff33ada9f809c0bbdf77a1f5cd6a09
fredmanre <fredmanre@gmail.com>
2019-05-25 14:36:33 -0400

    cambios y paso de parametros en el formulario(state change)

--- index.js
+++ index.js
@@ -4,6 +4,50 @@ import Hello from './Hello';
 import './style.css';
 
 
+class Formulario extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      email: '',
+      password: '',
+    }
+  }
+
+  syncChanges(value, property) {
+    let state = {}
+    state[property] = value
+    this.setState(state)
+  }
+
+  submitForm = () => {
+    console.log(this.state)
+  }
+
+  render() {
+    return (
+      <form>
+        <input 
+          onChange={(event) => {this.syncChanges(event.target.value, 'email')}}
+          type='email'
+          value={this.state.email}
+          placeholder='email'
+        />
+        <input 
+          onChange={(event) => {this.syncChanges(event.target.value, 'password')}}
+          type='password'
+          value={this.state.password}
+          placeholder='password'
+        />
+        <input 
+          onClick={this.submitForm}
+          type='submit'
+          value="iniciar sesion"
+        />
+      </form>
+    )
+  }
+}
+
 class Contador extends Component {
   constructor(props) {
     super(props)
@@ -31,6 +75,7 @@ class Contador extends Component {
         <p>{this.state.contador}</p>
         <button onClick={this.aumentar}>Aumentar</button>
         <button onClick={this.disminuir}>Disminuir</button>
+        <Formulario />
       </div>
     )
   }
